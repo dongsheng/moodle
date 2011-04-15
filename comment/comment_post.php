c324885c1546cb99d8929079a5242b8822dcf345
Sam Hemelryk <sam@moodle.com>
2011-04-15 14:59:33 +0800

    MDL-27038 comments Tidied up several areas of comments and implemented callback to ignore system permissions for view

--- comment/comment_post.php
+++ comment/comment_post.php
@@ -1,5 +1,4 @@
 <?php
-
 // This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
@@ -29,27 +28,28 @@ require_sesskey();
 
 $action    = optional_param('action',    '',     PARAM_ALPHA);
 $area      = optional_param('area',      '',     PARAM_ALPHAEXT);
-$commentid = optional_param('commentid', -1,     PARAM_INT);
 $content   = optional_param('content',   '',     PARAM_RAW);
 $itemid    = optional_param('itemid',    '',     PARAM_INT);
-$returnurl = optional_param('returnurl', '',     PARAM_URL);
+$returnurl = optional_param('returnurl', '/',     PARAM_URL);
 $component = optional_param('component', '', PARAM_ALPHAEXT);
 
-$cmt = new stdClass();
+// Currently this script can only add comments
+if ($action !== 'add') {
+    redirect($returnurl);
+}
+
+$cmt = new stdClass;
 $cmt->contextid = $contextid;
 $cmt->courseid  = $course->id;
+$cmt->cm        = $cm;
 $cmt->area      = $area;
 $cmt->itemid    = $itemid;
 $cmt->component = $component;
 $comment = new comment($cmt);
 
-switch ($action) {
-case 'add':
+if ($comment->can_post()) {
     $cmt = $comment->add($content);
     if (!empty($cmt) && is_object($cmt)) {
         redirect($returnurl);
     }
-    break;
-default:
-    exit;
 }
